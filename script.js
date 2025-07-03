0874fd8f4d6c87309f4bf21322e8f4ee2c98195f
Victor <victorsouzavieira.22@gmail.com>
2025-07-03 19:15:43 -0300

    Criar elementos os dados da API

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
    const body = document.querySelector('body')
    
-   async function creatElements(dados){
+   function creatElements(){
         let divContainer = document.createElement('div')
         divContainer.className = "cardsPokemons"
         let pictureImg = document.createElement('img')
@@ -25,33 +25,33 @@
     }
 
     async function getData (){
-    const url = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=$20"    
-        await fetch(url)
-        .then((response) =>{
-            console.log(response)
-            return response.json()
-        })
+        const url = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=$20"    
 
-        .then( async(data) => {
-            for (const pokemons of data.results) {
-                 await fetch(pokemons.url)
-                   .then((pokemonData) => {
-                        return pokemonData.json()
-                   })
-                   .then((dados) => {
-                    console.log(dados,'dados')
-                    let getName = dados.name
-                    //let acessarType = dados.type
-                    //let getType = dados.type
-                    let getId = dados.id
-                    console.log(getName)
-                    console.log(getId)
-                    //console.log(getType , 'getType')
-                    let getImg = document.getElementsByClassName("pokemonImg")
-                    getImg.src = dados.sprites['front_default']
-                   })
+            await fetch(url)
+    
+            .then((response) =>{
+             console.log(response)
+             return response.json()
+        })
+        .then(async(data) => {
+         for (const pokemons of data.results) {
+            await fetch(pokemons.url)
+                .then((pokemonData) => {
+                    return pokemonData.json()
+                    })
+                    
+                    .then((dados) => {
+                        console.log(dados,'dados')
+                        let getName = dados.name
+                        let getId = dados.id
+                        console.log(getName)
+                        console.log(getId)
+                        let getImg = document.getElementsByClassName("pokemonImg")
+                        getImg.src = dados.sprites['front_default']
+                })
             }
         })
+
         .catch((error) =>{
             console.log(error)
         })
