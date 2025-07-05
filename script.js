5558528cd86a4becb2fe7674b79d9f96a5f50dc0
Victor <victorsouzavieira.22@gmail.com>
2025-07-04 23:36:53 -0300

    mais um commit

--- script.js
+++ script.js
@@ -44,21 +44,23 @@ const body = document.querySelector('body')
                     console.log(dados,'dados')
                     let getName = dados.name
                     let getId = dados.id
+                    let getType = dados.types[0].type.name
                     let getImg = document.getElementsByClassName("pokemonImg")
                     getImg.src = dados.sprites['front_default']
                     console.log(getName)
                     console.log(getId)
-                   // console.log(getType)
+                    console.log(dados)
+                    console.log(getType , 'tipo')
 
-                    createElements(getName )
-
+                    //createElements(getName )
                     let pokemon = {
+                        img : getImg ,
                         name : getName,
-                        id : getId
-                     }
+                        id : getId,
+                        type : getType
+                     }              
 
-                     console.log(pokemon , "objeto")
-                    
+                     console.log(pokemon , "objeto") 
                 })
             }
         })
@@ -66,4 +68,25 @@ const body = document.querySelector('body')
             console.log(error)
         })
     }
+
+      const typeColors = {
+        normal: '#A8A77A',
+        fire: '#EE8130',
+        water: '#6390F0',
+        electric: '#F7D02C',
+        grass: '#7AC74C',
+        ice: '#96D9D6',
+        fighting: '#C22E28',
+        poison: '#A33EA1',
+        ground: '#E2BF65',
+        flying: '#A98FF3',
+        psychic: '#F95587',
+        bug: '#A6B91A',
+        rock: '#B6A136',
+        ghost: '#735797',
+        dragon: '#6F35FC',
+        dark: '#705746',
+        steel: '#B7B7CE',
+        fairy: '#D685AD'
+};
 
